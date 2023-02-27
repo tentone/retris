8f91b2e929ea8a57c308162d6dcd26a1b3f2bbca
Tentone <tentone@outlook.com>
2023-02-27 09:04:04 +0000

    Line

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ async fn main() {
     while running {
         clear_background(BLACK);
 
+        let width: f32 = screen_width();
+        let height: f32 = screen_height();
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(width / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(width - 30.0, height - 30.0, 15.0, YELLOW);
+
         
 
         next_frame().await;
